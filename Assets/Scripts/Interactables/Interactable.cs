d1961e662d138cc0ab8fa3f612cf08e3d25c03f4
miky <mikolas.fiser@gmail.com>
2025-10-09 16:26:52 +0200

    Input je řešen v updatu a ne v OnTriggerStay
    předtím ne vždy checkoval input, protože on trigger stay je ve physics update, tedy ne kazdy frame

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -5,55 +5,65 @@ public class Interactable : MonoBehaviour
 {
     public string interactTag = "Player";
     public KeyCode interactKey = KeyCode.F;
-    [Tooltip("If true, interaction requires pressing the 'F' key. If false, interaction happens automatically on trigger enter. /" +
-             "Jestliže je true, interakce vyžaduje stisknutí klávesy 'F'. Pokud je false, interakce probíhá automaticky při vstupu do triggeru.")]
+
+    [Tooltip("If true, interaction requires pressing the 'F' key. If false, interaction happens automatically on trigger enter.")]
     public bool inputInteraction = false;
+
     [Tooltip("Text, který se zobrazí, pokud musí hráč stisknout klávesu Interakce, pokud je prázdný, zobrazí se Press F")]
     public string hintText;
 
-    
-    public  void OnTriggerEnter2D(Collider2D other)
+    private bool playerInRange = false;
+    private Collider2D playerCollider;
+
+    void Update()
     {
-        if (!inputInteraction && other.CompareTag(interactTag))
+        if (inputInteraction && playerInRange && Input.GetKeyDown(interactKey))
+        {
+            Interact(playerCollider);
+        }
+
+        if (inputInteraction && playerInRange)
+        {
+            Interface.Instance.ShowHint($"Press {interactKey.ToString()} {hintText}");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(interactTag)) return;
+
+        if (!inputInteraction)
         {
             Debug.Log($"Player entered the interactable area of {gameObject}");
             Interact(other);
         }
-    }
-
-    public void OnTriggerStay2D(Collider2D other)
-    {
-        if (inputInteraction && other.CompareTag(interactTag))
+        else
         {
-            if (Input.GetKeyDown(interactKey))
-            {
-                Interact(other);
-            }
-         
-           Interface.Instance.ShowHint($"Press {interactKey.ToString()} {hintText}");
-            
+            playerInRange = true;
+            playerCollider = other;
         }
     }
-    
+
     public void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag(interactTag))
-        {
-            Interface.Instance.HideHint();
+        if (!other.CompareTag(interactTag)) return;
 
-            StopInteraction(other);
-        }
+        playerInRange = false;
+        playerCollider = null;
+
+        Interface.Instance.HideHint();
+        StopInteraction(other);
     }
 
     public virtual void Interact(Collider2D other)
     {
-        Debug.Log($"Player interacted with  {gameObject}");
+        Debug.Log($"Player interacted with {gameObject}");
         Interface.Instance.HideHint();
     }
 
     public virtual void StopInteraction(Collider2D other)
     {
-        Debug.Log($"Player stoped interaction with {gameObject}");
+        Debug.Log($"Player stopped interaction with {gameObject}");
     }
 
     public void OnDestroy()
